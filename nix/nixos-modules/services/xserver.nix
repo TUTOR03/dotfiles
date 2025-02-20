82675158e4daa2a8cc860350f5d4634529120f53
TUTOR03 <stukalov.dev@gmail.com>
2025-02-21 01:00:12 +0300

    fix: hardware settings for nvidia

--- nix/nixos-modules/services/xserver.nix
+++ nix/nixos-modules/services/xserver.nix
@@ -18,6 +18,27 @@
 
     # Указываем видеодрайвер
     services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+      # Использование проприетарного драйвера
+      open = false;
+
+      # Включение поддержки NVIDIA
+      modesetting.enable = true;
+
+      # Включение поддержки гибридной графики
+      prime = {
+        sync.enable = true;
+       
+        # Inter GPU
+        intelBusId = "PCI:0:2:0";
+        # NVIDIA GPU
+        nvidiaBusId = "PCI:2e:0:0";
+      };
+
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    services.xserver.videoDrivers = [ "nvidia" ];
 
     # Раскладка клавиатуры
     services.xserver.layout = "us,ru";
