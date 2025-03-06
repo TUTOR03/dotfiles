b9cbe33e4ab48e9ac7d7cfae5eef90b5afc5c9fa
TUTOR03 <stukalov.dev@gmail.com>
2025-03-07 01:41:22 +0300

    fix: remove overrides

--- home-modules/media/vlc.nix
+++ home-modules/media/vlc.nix
@@ -7,13 +7,7 @@
 
   config = lib.mkIf config.vlc.enable {
     home.packages = with pkgs; [
-      (vlc.override {
-        # Полная версия FFmpeg
-        ffmpeg = ffmpeg_7-full;
-        # Аппаратное ускорение для Intel/AMD/NVIDIA
-        libva = libva;
-        libvdpau = libvdpau;
-      })
+      vlc
     ];
   };
 }
