df29da26aceccc7a564b3a717aa0b0a88efabb3e
TUTOR03 <stukalov.dev@gmail.com>
2025-03-09 01:37:42 +0300

    fix: add some stuff to run hyprland on start

--- hosts/msi-laptop/configuration.nix
+++ hosts/msi-laptop/configuration.nix
@@ -58,6 +58,7 @@
 
   environment.systemPackages = with pkgs; [
     home-manager
+    kitty
   ];
 
   # Если хотим использовать XServer
@@ -68,6 +69,16 @@
     enable = true;
     package = pkgs.hyprland;
     xwayland.enable = true;
+  };
+  services.greetd = {
+    enable = true;
+    restart = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.hyprland}/bin/Hyprland";
+        user = "sdev";
+      };
+    };
   };
 
   # Включение сервисов из общих модулей
