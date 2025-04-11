50d5bfc82aed5a5196754e2a56dc85e88bf1a4c9
TUTOR03 <stukalov.dev@gmail.com>
2025-04-11 23:50:08 +0300

    feature: test config hyprland

--- home-modules/hyprland/hyprland.nix
+++ home-modules/hyprland/hyprland.nix
@@ -19,17 +19,38 @@
           "waybar"
         ];
         
+        bindm = [
+          "$mod, mouse:272, movewindow"
+          "$mod, mouse:273, resizewindow"
+          "$mod ALT, mouse:272, resizewindow"
+        ];
+        
         bind = [
-          "$mod, Q, exec, kitty"
-          "$mod, C, killactive"
-          "$mod, M, exit"
+          "$mod, Q, killactive"
+          "$mod, T, exec, kitty"
           "$mod, F, fullscreen"
         ];
 
         monitor = ",preferred,auto,1";
 
         general = {
-          border_size = 0;
+          border_size = 1;
+          gaps_in = 0;
+          gaps_out = 0;
+          gaps_workspaces = 0;
+          resize_on_border = true;
+          extend_border_grab_area = 10;
+          hover_icon_on_border = true;
+        };
+
+        decoration = {
+          blur = {
+            enabled = false;
+          };
+
+          shadow = {
+            enabled = false;
+          };
         };
       };
     };
