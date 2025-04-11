bd236ecafc324a8247367a0ee58819b89aa0ea19
TUTOR03 <stukalov.dev@gmail.com>
2025-04-12 00:20:10 +0300

    feature: some more config for hyprland and waybar

--- home-modules/hyprland/hyprland.nix
+++ home-modules/hyprland/hyprland.nix
@@ -29,6 +29,24 @@
           "$mod, Q, killactive"
           "$mod, T, exec, kitty"
           "$mod, F, fullscreen"
+          "$mod, 1, workspace, 1"
+          "$mod, 2, workspace, 2"
+          "$mod, 3, workspace, 3"
+          "$mod SHIFT, 1, movetoworkspace, 1"
+          "$mod SHIFT, 2, movetoworkspace, 2"
+          "$mod SHIFT, 3, movetoworkspace, 3"
+          "$mod ALT, left, workspace, -1"
+          "$mod ALT, right, workspace, +1"
+          "$mod, left, movefocus, l"
+          "$mod, down, movefocus, d"
+          "$mod, up, movefocus, u"
+          "$mod, right, movefocus, r"
+        ];
+
+        workspace = [
+          "1, default:true"
+          "2, default:true"
+          "3, default:true"
         ];
 
         monitor = ",preferred,auto,1";
@@ -53,6 +71,57 @@
           };
         };
       };
+    };
+
+    programs.waybar = {
+      enable = true;
+      settings = {
+        mainBar = {
+          layer = "top";
+          position = "top";
+          height = 30;
+
+          modules-left = [ "hyprland/workspaces" "hyprland/window" ];
+          modules-center = [ "clock" ];
+          modules-right = [ "cpu" "memory" "battery" "tray" ];
+          "hyprland/workspaces" = {
+            format = "{name}";
+            on-click = "activate";
+            persistent-workspaces = {
+              "*" = 3;
+            };
+          };
+          clock = {
+            format = "{:%d-%m-%Y %H:%M}";
+          };
+          cpu = {
+            format = "CPU {usage}%";
+          };
+          memory = {
+            format = "Mem {}%";
+          };
+          battery = {
+            format = "{capacity}% {icon}";
+            format-icons = [ "🔋" "🔌" ];
+          };
+        };
+      };
+      style = ''
+        * {
+          font-family: JetBrains Mono;
+          font-size: 13px;
+        }
+        #workspaces button {
+          padding: 0 5px;
+          color: #ffffff;
+        }
+        #workspaces button.active {
+          background: #646464;
+        }
+        #clock, #cpu, #memory, #battery, #tray {
+          padding: 0 10px;
+        }
+      '';
     };
   };
 }
