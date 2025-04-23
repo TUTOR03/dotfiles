44390e12af53b37cfab92de83d04854718c41969
TUTOR03 <stukalov.dev@gmail.com>
2025-04-23 15:48:45 +0300

    refactor: make home file more readable

--- hosts/msi-laptop/home.nix
+++ hosts/msi-laptop/home.nix
@@ -1,53 +1,22 @@
 { config, pkgs, ... }:
 
 {
+  # Пользовательские настройки
   home.username = "sdev";
   home.homeDirectory = "/home/sdev";
   home.stateVersion = "24.11";
+
+  home.sessionVariables = {
+    BROWSER = "google-chrome";
+    DEFAULT_BROWSER = "google-chrome";
+    NIXOS_OZONE_WL = "1";
+  };
 
   home.packages = with pkgs; [
     neofetch
     htop
     nerdfonts
   ];
-
-  # Включение ssh и git
-  ssh = {
-    enable = true;
-    keys = [
-      "~/.ssh/id_rsa_msi_laptop_github"
-    ];
-  };
-  git = {
-    enable = true;
-    sshKeyPathGithub = "~/.ssh/id_rsa_msi_laptop_github";
-    userName = "TUTOR03";
-    userEmail = "stukalov.dev@gmail.com";
-  };
-
-  # Включаем редакторы кода
-  vscode.enable = true;
-
-  obs.enable = true;
-  audacity.enable = true;
-  nomacs.enable = true;
-
-  # Включаем языки
-  nodejs = {
-    enable = true;
-    versions = with pkgs; [ nodejs_20 ];
-  };
-  rust.enable = true;
-
-  # Включаем браузеры
-  firefox.enable = true;
-  google-chrome.enable = true;
-
-  # Включаем VLC плеер
-  vlc.enable = true;
-
-  # Включаем Steam
-  steam.enable = true;
 
   programs.bash = {
     enable = true;
@@ -69,24 +38,17 @@
     };
   };
 
-  # Включаем Hyprland
-  hyprland.enable = true;
-
   fonts.fontconfig.enable = true;
 
-  # Настройка программ по умолчанию
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
-      # Видео открываются в VLC
       "video/mp4" = "vlc.desktop";
       "video/mpeg" = "vlc.desktop";
       "video/x-matroska" = "vlc.desktop";
       "video/webm" = "vlc.desktop";
       "video/avi" = "vlc.desktop";
       "video/quicktime" = "vlc.desktop";
-
-      # Браузер по умолчанию для HTTP/HTTPS/HTML
       "x-scheme-handler/http" = "google-chrome.desktop";
       "x-scheme-handler/https" = "google-chrome.desktop";
       "text/html" = "google-chrome.desktop";
@@ -94,10 +56,47 @@
     };
   };
 
-  # Переменные окружения
-  home.sessionVariables = {
-    BROWSER = "google-chrome";
-    DEFAULT_BROWSER = "google-chrome";
-    NIXOS_OZONE_WL = "1";
+  # Подключение модулей
+
+  # Настройка SSH и Git
+  ssh = {
+    enable = true;
+    keys = [
+      "~/.ssh/id_rsa_msi_laptop_github"
+    ];
   };
+  git = {
+    enable = true;
+    sshKeyPathGithub = "~/.ssh/id_rsa_msi_laptop_github";
+    userName = "TUTOR03";
+    userEmail = "stukalov.dev@gmail.com";
+  };
+
+  # Редакторы кода
+  vscode.enable = true;
+
+  # Языки программирования
+  nodejs = {
+    enable = true;
+    versions = with pkgs; [ nodejs_20 ];
+  };
+  rust.enable = true;
+
+  # Браузеры
+  firefox.enable = true;
+  google-chrome.enable = true;
+
+  # Медиа программы
+  vlc.enable = true;
+  audacity.enable = true;
+  nomacs.enable = true;
+
+  # Игры
+  steam.enable = true;
+
+  # Рабочий стол
+  hyprland.enable = true;
+
+  # Прочие программы
+  obs.enable = true;
 }
