7006ca90e82008174fdfdb91c32dca8b66190995
Stukalov Artem <stukalov.dev@gmail.com>
2025-07-02 00:57:30 +0300

    feature: enable networking

--- hosts/sdev-pc/networking.nix
+++ hosts/sdev-pc/networking.nix
@@ -4,6 +4,7 @@
   networking = {
     networkmanager.enable = true;
     useDHCP = lib.mkDefault true;
-    # interfaces.wlo1.useDHCP = lib.mkDefault true;
+    interfaces.enp12s0.useDHCP = lib.mkDefault true;
+    interfaces.wlp13s0.useDHCP = lib.mkDefault true;
   };
 }
