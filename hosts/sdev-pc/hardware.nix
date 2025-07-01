a8b9d3aa49ea05ee390d2c400d40a8600fd4c012
Stukalov Artem <stukalov.dev@gmail.com>
2025-07-02 01:02:25 +0300

    Update hardware.nix

--- hosts/sdev-pc/hardware.nix
+++ hosts/sdev-pc/hardware.nix
@@ -7,26 +7,14 @@
       enable32Bit = true;
     };
 
-    # opengl.enable = true;
+    opengl.enable = true;
 
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-    # nvidia = {
-    #   open = false;
-    #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-    #   modesetting.enable = true;
-    #   powerManagement.enable = true;
-    #   powerManagement.finegrained = true;
-    #   nvidiaSettings = true;
-
-    #   prime = {
-    #     offload = {
-    #       enable = true;
-    #       enableOffloadCmd = true;
-    #     };
-    #     intelBusId = "PCI:0:2:0";
-    #     nvidiaBusId = "PCI:46:0:0";
-    #   };
-    # };
+    nvidia = {
+      open = false;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      nvidiaSettings = true;
+    };
   };
 }
